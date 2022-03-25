6868dea5bdab9cd6d180041dfcd826c1e8684efd
soysushi <chi_oscar@hotmail.com>
2022-03-24 22:08:59 -0700

    signing off

--- users/admin.py
+++ users/admin.py
@@ -3,16 +3,11 @@ from django.contrib.auth.models import User
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
 from rest_framework_api_key.admin import APIKeyModelAdmin
-from .models import CustomUserAPIKey
 
 CustomUser = get_user_model()
 
-@admin.register(CustomUserAPIKey)
-class CustomUserAPIKeyModelAdmin(APIKeyModelAdmin):
-    pass
-
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
-    list_display = ['email', 'api_keys']
+    list_display = ['uuid', 'email', 'api_key']
 
 admin.site.register(CustomUser, CustomUserAdmin)
--- users/models.py
+++ users/models.py
@@ -1,17 +1,11 @@
 # Create your models here.
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from rest_framework_api_key.models import AbstractAPIKey
+from rest_framework_api_key.permissions import HasAPIKey
 import uuid
 
 
 class CustomUser(AbstractUser):
     uuid = models.UUIDField(default=uuid.uuid4, primary_key=True, unique=True)
     email = models.EmailField(unique=True)
-
-class CustomUserAPIKey(AbstractAPIKey):
-    user = models.ForeignKey(
-        CustomUser,
-        on_delete=models.CASCADE,
-        related_name="api_keys",
-    )
+    api_key = [HasAPIKey]
