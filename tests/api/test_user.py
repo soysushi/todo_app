66d7a7d81ab7a5623f68c843af5a62160830e23d
soysushi <chi_oscar@hotmail.com>
2022-03-28 16:21:54 -0700

    test_user

--- tests/api/test_user.py
+++ tests/api/test_user.py
@@ -4,3 +4,15 @@
 # attach api key to request, login the user
 # while logged in
     # perform API requests
+
+import pytest
+from rest_framework.test import APIClient
+
+client = APIClient()
+
+def test_new_user(django_user_model):
+    test_user = django_user_model.objects.create(username="someone", password="something")
+    client.force_login(test_user)
+    response = client.post('api/token', test_user)
+    data = response
+    assert response.status_code == 404
